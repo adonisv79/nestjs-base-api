ab7adcfdb92b90cfba92ae0c1c2ae60c8dc6f5f5
Adonis Lee Villamor <adonisv79@gmail.com>
2025-02-22 16:17:23 +0800

    Revert "20250216 add redis game session (#5)"
    This reverts commit a4468cbfb8a99ad56bc92f40326f22e8ec3d3ee6.

--- src/common/errors/user_not_found.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorData, OpenTCGError } from './open_tcg_errors';
-
-export class UserNotFoundError extends OpenTCGError {
-  constructor(data: ErrorData) {
-    super('User Not Found', 'USR404', data);
-  }
-}
